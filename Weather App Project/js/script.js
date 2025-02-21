38906adee60734ee78dc83c2ef351a23ca55f69b
Manikandan M <manimani10aug2004@gmail.com>
2025-02-21 12:31:31 +0530

    Add files via upload

---
+++ Weather App Project/js/script.js
@@ -0,0 +1,35 @@
+const apiKey = "3cbeb3258a1022ea1211b11439f2bc86";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  var data = await response.json();
+
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+  if (data.weather[0].main == "Clouds") {
+    weatherIcon.src = "img/clouds.png";
+  } else if (data.weather[0].main == "Clear") {
+    weatherIcon.src = "img/clear.png";
+  } else if (data.weather[0].main == "Rain") {
+    weatherIcon.src = "img/rain.png";
+  } else if (data.weather[0].main == "Drizzle") {
+    weatherIcon.src = "img/drizzle.png";
+  } else if (data.weather[0].main == "Mist") {
+    weatherIcon.src = "img/mist.png";
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
